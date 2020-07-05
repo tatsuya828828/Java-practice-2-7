8c7b0ef0bbc53a0d42fb1dd5795d7b0adc653531
tatsuya828828 <kkjy011894@gmail.com>
2020-07-05 16:47:42 +0900

    [finish]7-4

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,23 @@
+import java.util.Iterator;
+import java.util.Properties;
 
 public class Main {
 	public static void main(String[] args) {
-		// データが正しくない時
-		int a = 3;
-		int b = 2;
-		int c = a-b;
-		if(c == 8) {
-			System.out.println("答えが間違っています。異常終了します");
-			System.exit(1);
+		System.setProperty("rpg.version", "0.3");
+		System.setProperty("rpg.author", "たかし");
+		String ver  = System.getProperty("rpg.version");
+		String author = System.getProperty("rpg.author");
+		System.out.println("RPG: スッキリ魔王征伐 ver" + ver);
+		System.out.println("Developed by" + author);
+		System.out.print("ご利用のJavaバージョン:");
+		System.out.println(System.getProperty("java.version"));
+		Properties p = System.getProperties();
+		Iterator<String> i = p.stringPropertyNames().iterator();
+		System.out.println("[システムプロパティ一覧]");
+		while(i.hasNext()) {
+			String key = i.next();
+			System.out.print(key + "=");
+			System.out.println(System.getProperty(key));
 		}
-		System.out.println("正常終了しました");
 	}
 }
