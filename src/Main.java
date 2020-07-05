ae86518a309a2a4886dcf36583a1cf4f9f13a589
tatsuya828828 <kkjy011894@gmail.com>
2020-07-05 17:00:16 +0900

    [finish]7-6

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
 	public static void main(String[] args) {
-		long f = Runtime.getRuntime().freeMemory()/ 1024/ 1024;
-		System.out.println(f);
+		Class<?> info1 = String.class;
+		System.out.println(info1.getSimpleName());
+		System.out.println(info1.getName());
+		System.out.println(info1.getPackage().getName());
+
+		System.out.println(info1.isArray());
+		Class<?> info2 = info1.getSuperclass();
+		System.out.println(info2.getName());
+		Class<?> info3 = args.getClass();
+		System.out.println(info3.isArray());
 	}
 }
